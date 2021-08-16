5e2429d98e2306ca4f391d0a30e69a0413bdf155
deverca <deverca@oocl.com>
2021-08-16 20:36:22 +0800

    [COUNTER[Carmela]feat: added an updateCounterSize function in MultipleCounter

--- src/Components/MultipleCounter.js
+++ src/Components/MultipleCounter.js
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import Counter from "./Counter";
 import CounterGroup from "./CounterGroup";
 import CounterSizeGenerator from "./CounterSizeGenerator";
 
 function MultipleCounter(){
 
+const[counterSize, setCounterSize] = useState(0);
+
+function updateCounterSize(counterSize){
+    setCounterSize(counterSize);
+}
 
 
 return(
 <div>
-<CounterSizeGenerator></CounterSizeGenerator>
-<CounterGroup></CounterGroup>
+<CounterSizeGenerator updateCounterSize= {updateCounterSize}></CounterSizeGenerator>
+<CounterGroup counterSize={counterSize}></CounterGroup>
 </div>
 
 );
